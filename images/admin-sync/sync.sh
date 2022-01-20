9b927c737e8594dd2341eb863b30bc825a2665c4
Steven Stetzler <steven.stetzler@gmail.com>
2022-01-20 01:07:28 +0000

    remove old images from admin/image consolidation task

--- images/admin-sync/sync.sh
+++
@@ -1,2 +0,0 @@
-# Get users
-users_and_admins=$(curl -s -H "Authorization: token ${JUPYTERHUB_API_TOKEN}" hub:8081/hub/api/users | jq -r '.[] | "\(.name) \(.admin)"')
--- images/base/scripts/pre-start-source.sh
+++
@@ -1,4 +0,0 @@
-# Automatically create JAVA_HOME
-export JAVA_HOME="$(dirname $(dirname $(readlink -f $(which java))))"
-# Add py4j libraries to pythonpath
-export PYTHONPATH=$SPARK_HOME/python:$(ls $SPARK_HOME/python/lib/py4j*.zip)
