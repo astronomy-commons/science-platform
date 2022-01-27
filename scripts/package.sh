5939aec38ce4f82d01ef20815a885d8aaa64615d
Steven Stetzler <steven.stetzler@gmail.com>
2022-01-27 00:26:33 +0000

    keep NFS inside chart for now, but modify storage class name to prevent conflict of test deployments

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# Switch to deployment directory and load the config
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$(dirname $DIR)"
+. scripts/common.sh
+
+test_dependency helm
 
 # Create a new chart archive versioned
 # Chart version depends on the value in Chart.yaml
